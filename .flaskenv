9d9d77e1543316a50477e57f5dac4e3f659d5df7
nancy <nanx.rao@gmail.com>
2024-05-10 20:06:28 +0800

    correction

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=communityboard.py
-FLASK_ENV=development
--- app/config.py
+++
@@ -1,8 +0,0 @@
-# config for sqlite database
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join( basedir,'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
