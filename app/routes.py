0167cda169f5879af14f2ddc91f60bd9fcb63437
ivy <ivyhzyb@gmail.com>
2024-05-11 10:43:45 +0800

    Fixed: redirection to the create request page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import jsonify, render_template
+from flask import jsonify, render_template, redirect,url_for
 from app import app
+from app.model import User,Tag,Request
 from flask import request
 
 user = {'username': 'Admin'}
@@ -28,7 +29,13 @@ def ViewRequest():
 
 @app.route('/create-request')
 def CreateRequest():
-    return render_template("create-request.html", title="Create the request")
+    return render_template("create-request.html", title="Create the request",Request=[])
+
+# submit page to confirm the submitting of new request
+@app.route('/submit',methods=['post'])
+def Submit():
+    print('Submitted!')
+    return redirect(location = url_for('ViewRequest'))
 
 @app.route('/search', methods=['POST'])
 def search():
